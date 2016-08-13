5c964f2940930bff768c384ac7f8fb3567a45114
rohullah <rohullahalavi@gmail.com>
2016-08-13 16:08:32 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+rohullah<br><b>alavi
+    <br>
+<?php
+$bbc = 23;
+$jzk = 34;
+$ali = $bbc+$jzk;
+//for ($i=1;$i<=30;$i++) {
+//    echo "<br>";
+//    echo "<b>hello"."  ".$i%3 ." ". $i;
+//}
+while ($ali >0){
+    echo $ali . "<br>";
+    $ali --;
+}
+?>
